512b6153603a7596a39592272879c00fe17d8e8d
Sal Savage <salsavage@google.com>
2022-04-15 15:38:17 -0700

    polaris: Replace BTM_DEF_LOCAL_NAME with a sysprop
    Change-Id: I2c90efac01e61500eb1be23bb0ae6f96c2acdc53

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,5 +25,15 @@ TARGET_KERNEL_CONFIG += xiaomi/sirius.config
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
 # Inherit from the proprietary version
 include vendor/xiaomi/sirius/BoardConfigVendor.mk
