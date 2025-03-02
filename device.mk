ff479eae197f2680152d9e06ad1c79f06f36cc81
nara atthama <pancasila.day@gmail.com>
2025-03-02 19:18:40 +0000

    sirius: Set default hotspot ssid
    Added a device specific overlay that changes the default name
    from "AndroidAp" to customized name
    
    Test: Builds, manually verified name change
    Bug: 36368950
    Change-Id: I38a5f6fb950d6a45342080bae186a8ed276d94bf

--- device.mk
+++ device.mk
@@ -45,5 +45,12 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
 
+# Secure element
+PRODUCT_PACKAGES += \
+    SecureElement
+
+# WiFi
+PRODUCT_PACKAGES += \
+    TargetWifiOverlay
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sirius/sirius-vendor.mk)
